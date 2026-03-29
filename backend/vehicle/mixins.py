7041ac4bf816e622ca0d9794eb282af2d91a1712
Radomir Mijovic <batomijovic4@gmail.com>
2026-03-29 19:19:31 +0200

    chore: cleanup and mixin

--- backend/vehicle/mixins.py
+++ backend/vehicle/mixins.py
@@ -1,1 +1,35 @@
-class VehicleViewMixin: ...
+from django.contrib import messages
+from django.http import HttpResponse
+
+
+class VehicleViewMixin:
+    def _get_hx_field_errors(self, form):
+        errors = []
+        for field, errs in form.errors.items():
+            label = form.fields[field].label if field in form.fields else field
+            for error in errs:
+                errors.append(f"{label}: {error}")
+        return errors
+
+    def _return_partials_http_response(self, errors):
+        return HttpResponse(b", ".join(errors), status=422)
+
+    def _handle_form_partials_errors(self, form):
+        errors = self._get_hx_field_errors(form)
+        return self._return_partials_http_response(errors)
+
+    def _handle_form_errors(self, form):
+        for field, errors in form.errors.items():
+            for error in errors:
+                label = self._generate_labels(form, field)
+                self._self_message_error(label, error)
+
+    def _generate_labels(self, form, field):
+        label = form.fields[field].label if field in form.fields else field
+        return label
+
+    def _self_message_error(self, label: str, error: str) -> None:
+        messages.error(self.request, f"{label}: {error}", extra_tags="vehicle-error")
+
+    def _self_message_success(self, msg: str) -> None:
+        messages.success(self.request, msg)
--- backend/vehicle/views.py
+++ backend/vehicle/views.py
@@ -1,4 +1,3 @@
-from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import (
     HttpResponse,
@@ -10,6 +9,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import VehicleCreateForm
+from .mixins import VehicleViewMixin
 from .models import Vehicle
 
 
@@ -33,7 +33,7 @@ class VehicleDetailView(LoginRequiredMixin, generic.DetailView):
         ).select_related("company")
 
 
-class VehicleCreateView(LoginRequiredMixin, generic.CreateView):
+class VehicleCreateView(LoginRequiredMixin, VehicleViewMixin, generic.CreateView):
     model = Vehicle
     form_class = VehicleCreateForm
     success_url = reverse_lazy("vehicle:vehicles")
@@ -49,44 +49,27 @@ class VehicleCreateView(LoginRequiredMixin, generic.CreateView):
 
     def form_invalid(self, form):
         if self.request.headers.get("HX-Request"):
-            errors = []
-            for field, errs in form.errors.items():
-                label = form.fields[field].label if field in form.fields else field
-                for error in errs:
-                    errors.append(f"{label}: {error}")
-            return HttpResponse(b", ".join(errors), status=422)
+            return self._handle_form_partials_errors(form)
 
         self._handle_form_errors(form)
         return redirect("vehicle:vehicles")
 
     def _message_and_render_partials(self, vehicle: Vehicle) -> HttpResponse:
-        self._self_message_success()
-        return render(self.request, "partials/vehicle_row.html", {"vehicle": vehicle})
+        self._self_message_success("Vozilo je uspješno dodano.")
+        return render(
+            self.request,
+            "partials/vehicle_row.html",
+            {"vehicle": vehicle, "include_modal": True},
+        )
 
     def _message_and_render_template(
         self,
     ) -> HttpResponsePermanentRedirect | HttpResponseRedirect:
-        self._self_message_success()
+        self._self_message_success("Vozilo je uspješno dodano.")
         return redirect("vehicle:vehicles")
 
-    def _handle_form_errors(self, form):
-        for field, errors in form.errors.items():
-            for error in errors:
-                label = self._generate_labels(form, field)
-                self._self_message_error(label, error)
 
-    def _generate_labels(self, form, field):
-        label = form.fields[field].label if field in form.fields else field
-        return label
-
-    def _self_message_error(self, label: str, error: str) -> None:
-        messages.error(self.request, f"{label}: {error}", extra_tags="vehicle-error")
-
-    def _self_message_success(self) -> None:
-        messages.success(self.request, "Vozilo je uspješno dodano.")
-
-
-class VehicleUpdateView(LoginRequiredMixin, generic.UpdateView):
+class VehicleUpdateView(LoginRequiredMixin, VehicleViewMixin, generic.UpdateView):
     model = Vehicle
     form_class = VehicleCreateForm
 
@@ -101,35 +84,15 @@ class VehicleUpdateView(LoginRequiredMixin, generic.UpdateView):
 
     def form_invalid(self, form):
         if self.request.headers.get("HX-Request"):
-            errors = self._get_hx_field_errors(form)
-            return HttpResponse(b", ".join(errors), status=422)
+            return self._handle_form_partials_errors(form)
 
-        for field, errors in form.errors.items():
-            for error in errors:
-                label = form.fields[field].label if field in form.fields else field
-                messages.error(
-                    self.request, f"{label}: {error}", extra_tags="vehicle-error"
-                )
-
+        self._handle_form_errors(form)
         vehicle = self.get_object()
         return redirect("vehicle:vehicles_details", pk=vehicle.id)
 
     def _get_partial_template(self):
-        template = (
-            "partials/vehicle_details_row.html"
-            if self.request.POST.get("source") == "details"
-            else "partials/vehicle_row.html"
-        )
+        template = "partials/vehicle_details_row.html"
         return template
-
-    @staticmethod
-    def _get_hx_field_errors(form):
-        errors = []
-        for field, errs in form.errors.items():
-            label = form.fields[field].label if field in form.fields else field
-            for error in errs:
-                errors.append(f"{label}: {error}")
-        return errors
 
 
 class VehicleDeleteView(LoginRequiredMixin, generic.DeleteView):
