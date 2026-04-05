6b85d53945b9d03c4be413b7d23142f567d1e2ce
Radomir Mijovic <batomijovic4@gmail.com>
2026-04-05 13:51:57 +0200

    chore: typo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS ", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 INSTALLED_APPS = [
