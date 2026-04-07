5e73ebd0d04f1289cf18ce33c82ceebd2269f295
Radomir Mijovic <batomijovic4@gmail.com>
2026-04-07 10:22:09 +0200

    chore: prod settings

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
