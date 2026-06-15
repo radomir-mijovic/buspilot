0daf436eeed1cf4fc42a42c7ab5819ecc1fb33c7
Radomir Mijovic <batomijovic4@gmail.com>
2026-06-15 10:08:18 +0200

    feat: request demo mobile version

--- backend/assets/components/requestDemo/RequestDemo.jsx
+++ backend/assets/components/requestDemo/RequestDemo.jsx
@@ -1,10 +1,12 @@
 import styles from "./RequestDemo.module.scss";
 import RequestDemoCard from "./RequestDemoCard";
+import RequestDemoMobileCard from "./RequestDemoMobileCard";
 
 const RequestDemo = () => {
   return (
     <div className={styles.pageWrapper}>
       <RequestDemoCard />
+      <RequestDemoMobileCard />
     </div>
   );
 };
---
+++ backend/assets/components/requestDemo/RequestDemoMobileCard.jsx
@@ -0,0 +1,43 @@
+import styles from "./RequestDemoMobileCard.module.scss";
+import brandLogo from "../../icons/buspilot-favicon.svg";
+import RequestDemoForm from "./RequestDemoForm";
+import { useState } from "react";
+
+const RequestDemoMobileCard = () => {
+  const [response, setResponse] = useState("");
+
+  return (
+    <div className={styles.mobileCard}>
+      <div className={styles.cardHeader}>
+        <img src={brandLogo} alt="" width={20} height={20} />
+        <h3 className="font-display">BusPilot</h3>
+      </div>
+      {response ? (
+        <>
+          <h4>{response}</h4>
+          <a href="/">Go to website</a>
+        </>
+      ) : (
+        <>
+          <div className={styles.lowerTextWrapper}>
+            <h5>request a demo</h5>
+            <h2>See BusPilot in action</h2>
+            <p>
+              Tell us a bit about your company and we'll set up a personalized
+              walkthrough.
+            </p>
+          </div>
+          <RequestDemoForm setResponse={setResponse} />
+          <div className={styles.lowerPWrapper}>
+            <p>We'll get back to you within one business day.</p>
+            <p>
+              Already have an account? <a href="/login">Sign in</a>
+            </p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default RequestDemoMobileCard;
